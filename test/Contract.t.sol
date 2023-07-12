8d047517033789db8eb7629ff8ba70d31d005da3
jnbissu589@gmail.com <jnbissu589@gmail.com>
2023-07-12 09:51:36 -0400

    init

--- test/Contract.t.sol
+++ test/Contract.t.sol
@@ -2,11 +2,26 @@
 pragma solidity ^0.8.13;
 
 import "forge-std/Test.sol";
+import "./CoinFlipContract.sol";
 
-contract ContractTest is Test {
-    function setUp() public {}
+contract CoinFlipTest {
+    CoinFlip public coinFlipContract;
 
-    function testExample() public {
-        assertTrue(true);
+    constructor(address _coinFlipAddress) {
+        coinFlipContract = CoinFlip(_coinFlipAddress);
+    }
+
+    function testWinStreak() public {
+        bool guess = true;
+
+        for (uint256 i = 0; i < 10; i++) {
+            bool result = coinFlipContract.flip(guess);
+
+            // Verify that the guess was correct
+            require(result == true, "Incorrect guess");
+
+            // Verify the consecutive win streak count
+            require(coinFlipContract.consecutiveWins() == i + 1, "Incorrect win streak count");
+        }
     }
 }
