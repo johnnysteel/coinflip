82ba661a19bfa10ca97854b73530cc95f9f72166
jnbissu589@gmail.com <jnbissu589@gmail.com>
2023-07-12 10:02:36 -0400

    import change

--- test/Contract.t.sol
+++ test/Contract.t.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.13;
 
 import "forge-std/Test.sol";
-import "./CoinFlipContract.sol";
+import "src/CoinFlipContract.sol";
 
 contract CoinFlipTest {
     CoinFlip public coinFlipContract;
